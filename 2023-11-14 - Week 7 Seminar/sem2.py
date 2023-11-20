753f60bbbdf2f88f283716102c3c03c6f9ecb522
zydezu <kinomaxxing@gmail.com>
2023-11-20 18:27:56 +0000

    itworks

--- 2023-11-14 - Week 7 Seminar/sem2.py
+++ 2023-11-14 - Week 7 Seminar/sem2.py
@@ -1,17 +1,17 @@
-def wildcard_pattern(input):
-    print(input)
+def wildcard_pattern(pattern):
+    all_patterns = set()
+    wildcard_rec(pattern, '', all_patterns)
+    return all_patterns
 
-    if "?" not in input:
-        return input
-
-    solutions = []
-
-    if input[0] == "?":
-        solutions.append(wildcard_pattern("0" + input[1:]))
-        solutions.append(wildcard_pattern("1" + input[1:]))
+def wildcard_rec(pattern, under_construction, binaries):
+    if pattern == "":
+        binaries.add(under_construction)
+    elif pattern[0] != "?":
+        wildcard_rec(pattern[1:], under_construction + pattern[0], binaries)
     else:
-        solutions.append(wildcard_pattern(input[1:]))
+        wildcard_rec(pattern[1:], under_construction + "0", binaries)
+        wildcard_rec(pattern[1:], under_construction + "1", binaries)
 
-    return solutions
 
-print(wildcard_pattern("1?11"))
+
+print(wildcard_pattern("??0?001"))
